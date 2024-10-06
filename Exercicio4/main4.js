9dd77ddf8758ecbf033d4b5c43f2ea1a52477057
linconvinicius <lvini_@live.com>
2024-10-05 22:32:45 -0300

    resposta exercicio 4

---
+++ Exercicio4/main4.js
@@ -0,0 +1,38 @@
+const DatabaseError = function (statement, message) {
+    this.statement = statement;
+    this.message = message;
+};
+const database = {
+    tables: {},
+    createTable(statement) {
+        const regExp = /create table (\w+) \((.+)\)/;
+        const parsedStatement = statement.match(regExp);
+        const tableName = parsedStatement[1];
+        this.tables[tableName] = {
+            columns: {},
+            data: []
+        };
+        let columns = parsedStatement[2];
+        columns = columns.split(",");
+        for (let column of columns) {
+            column = column.trim().split(" ");
+            const name = column[0];
+            const type = column[1];
+            this.tables[tableName].columns[name] = type;
+        }
+    },
+    execute(statement) {
+        if (statement.startsWith("create table")) {
+            return this.createTable(statement);
+        }
+        const message = `Syntax error: "${statement}"`;
+        throw new DatabaseError(statement, message);
+    }
+};
+try {
+    database.execute("create table author (id number, name string, age number, city string, state string, country string)");
+    console.log(JSON.stringify(database, undefined, "  "));
+    database.execute("select id, name from author");
+} catch (e) {
+    console.log(e.message);
+};
